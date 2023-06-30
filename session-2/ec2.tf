72547eb78f53c5ba126e79fb9b1c467716bbb0b7
tugba9495 <tuba_7655@icloud.com>
2023-06-30 01:14:55 -0500

    modified ec2.tf

--- session-2/ec2.tf
+++ session-2/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "my_instance" {
-    ami = "ami-04ceee5659211731a"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "myinstance"
-    }
+  ami           = "ami-04ceee5659211731a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "myinstance"
+  }
+  vpc_security_group_ids = [aws_security_group.my_security_group3.id]
 
 }
