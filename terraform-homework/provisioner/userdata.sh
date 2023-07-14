a865fc6817d22f1f0513638cb05eae427e733f81
tugba9495 <tuba_7655@icloud.com>
2023-07-14 00:53:45 -0500

    changed userdata.sh

--- terraform-homework/provisioner/userdata.sh
+++ terraform-homework/provisioner/userdata.sh
@@ -1,6 +1,6 @@
 
-    #!/bin/bash
-    sudo apt install httpd -y
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
-    sudo echo "hello world!" > /var/www/html/index.html
+    # #!/bin/bash
+    # sudo apt install httpd -y
+    # sudo systemctl start httpd
+    # sudo systemctl enable httpd
+    # sudo echo "hello world!" > /var/www/html/index.html
